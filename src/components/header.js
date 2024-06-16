ba0d4c97bd32403c5e030e1061b7d4899ff728ca
AgileWebDev92 <billpaxton92@gmail.com>
2024-06-17 01:48:09 +0900

    header for laptop

--- src/components/header.js
+++ src/components/header.js
@@ -1,47 +1,75 @@
-import {useEffect, useState} from 'react';
-import Logo from '../assets/img/Logo.svg';
-import sLogo from '../assets/img/sm_Logo.svg';
+import { useEffect, useRef, useState } from "react";
+import Logo from "../assets/img/logo.svg";
 
-const Header = ({setNavbar}) => {
-
+const Header = ({ visibility, setNavbar }) => {
+  console.log(visibility);
   return (
-    <>
-      <div className='w-screen hidden lg:flex flex-row items-center justify-between h-36 pt-[72px] pb-8 xl:px-8 px-[69px]'>
-        <div className='flex flex-row pl-6 xl:pl-10 items-center'>
-          <img src={Logo} className='w-48' alt='logo' />
+    <div className="flex flex-col">
+      <div className="w-screen flex flex-row justify-between h-full">
+        <div className="flex flex-col w-full">
+          <div className="flex flex-row justify-between items-center h-[62px] sm:h-[92px] px-5 py-2.5 sm:py-5">
+            <img src={Logo} alt="logo" className="w-[34px] h-[42px]" />
+            <button className="anim-button" onClick={setNavbar}>
+              <div className={`${visibility ? "invisible" : ""}`}></div>
+              <div></div>
+              <div className={`${visibility ? "invisible" : ""}`}></div>
+            </button>
+          </div>
+          <div
+            className={`flex flex-grow flex-col pl-[250px] overflow-y-auto header${
+              visibility ? "-expanded" : ""
+            }`}
+          >
+            <div className="flex flex-col pt-5">
+              <a href="#" className="nav-text-item">
+                About
+              </a>
+              <div className="h-[1px] bg-white opacity-10"></div>
+            </div>
+            <div className="flex flex-col pt-5">
+              <a href="#" className="nav-text-item">
+                Marketplace
+              </a>
+              <div className="h-[1px] bg-white opacity-10"></div>
+            </div>
+            <div className="flex flex-col pt-5">
+              <a href="#" className="nav-text-item">
+                Features
+              </a>
+              <div className="h-[1px] bg-white opacity-10"></div>
+            </div>
+            <div className="flex flex-col pt-5">
+              <a href="#" className="nav-text-item">
+                Contact Us
+              </a>
+              <div className="h-[1px] bg-white opacity-10"></div>
+            </div>
+            <div className="flex flex-row pt-11 justify-between items-center font-sfui">
+              <span className="font-semiold text-2xl text-gold leading-[28.8px]">
+                Subscribe for News, Updates and Articles
+              </span>
+              <div className="flex flex-row"></div>
+              <div className="flex flex-row p-1 border-solid border-[1px] border-hint-text rounded-md justify-between">
+                <input
+                  type="email"
+                  placeholder="E-mail"
+                  className="text-xl leading-6 p-4 text-white placeholder:text-hint-text"
+                />
+                <button className="bg-white font-bold text-base leading-4 px-6 py-4 text-black rounded">
+                  SUBSCRIBE NOW
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className='flex flex-row flex-1 items-center justify-around xl:justify-center xl:gap-16'>
-          <span>
-            <a href='#home' className='text-4xl font-medium'>Home</a>
-          </span>
-          <span>
-            <a href='#explore' className='text-4xl font-medium'>Explorer</a>
-          </span>
-          <span>
-            <a href='#' className='text-4xl leading-8 font-medium'>Collections</a>
-          </span>
-          <span>
-            <a href='#' className='text-4xl font-medium'>Launchpad</a>
-          </span>
-          <span>
-            <a href='#' className='text-4xl font-medium'>Mint</a>
-          </span>
-        </div>
-        <button className='flex items-center font-sfui font-bold rounded text-dark-text text-lg leading-[18px] bg-gold px-8 xl:px-[74px] py-4'>CONNECT WALLET</button>
-      </div>
-      <div className='w-screen flex flex-row lg:hidden justify-between items-center h-16 px-5 py-2.5'>
-        <img src={sLogo} alt='logo' className='w-[34px] h-[42px]' />
-        <div className='flex flex-row gap-5 h-full items-center'>
-          <button className='bg-gold h-full font-sfui font-bold text-sm leading-[14px] px-6 rounded text-dark-text'>CONNECT WALLET</button>
-          <button className='anim-button' onClick={setNavbar}>
-            <div></div>
-            <div></div>
-            <div></div>
+        <div className="flex flex-col shrink-0 gap-5 items-center px-5 py-2.5 sm:py-5">
+          <button className="bg-gold h-full font-sfui font-bold text-sm leading-[14px] px-6 rounded text-dark-text">
+            VIEW PRESENTATION
           </button>
         </div>
       </div>
-    </>
+    </div>
   );
-}
+};
 
 export default Header;
